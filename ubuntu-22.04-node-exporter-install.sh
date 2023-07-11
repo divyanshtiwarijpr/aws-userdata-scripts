416a67fd9aab36b03ba53a01fd0678ddbf526073
Divyansh Tiwari <divyanshtiwarijpr@gmail.com>
2023-07-11 15:03:24 +0530

    Create ubuntu-22.04-node-exporter-install.sh

---
+++ ubuntu-22.04-node-exporter-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /tmp
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.0/node_exporter-1.6.0.linux-amd64.tar.gz
+tar -xvf node_exporter-1.6.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.6.0.linux-amd64/node_exporter /usr/local/bin/
+sudo useradd --system --shell /bin/false node_exporter
+sudo tee /etc/systemd/system/node_exporter.service << EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now node_exporter
+
