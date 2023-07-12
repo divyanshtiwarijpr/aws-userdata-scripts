f7e03076c535f2d8f99e4512616992291438df71
Divyansh <divyanshtiwarijpr@gmail.com>
2023-07-12 14:22:09 +0530

    Update

--- ubuntu-22.04-node-exporter-install.sh
+++ ubuntu-22.04-node-exporter-install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+sudo apt install apache2 -y
 cd /tmp
+
+# Download the node_exporter zip file then extract it and then move the binary to proper location 
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.0/node_exporter-1.6.0.linux-amd64.tar.gz
 tar -xvf node_exporter-1.6.0.linux-amd64.tar.gz
 sudo mv node_exporter-1.6.0.linux-amd64/node_exporter /usr/local/bin/
 sudo useradd --system --shell /bin/false node_exporter
 sudo tee /etc/systemd/system/node_exporter.service << EOF
 [Unit]
-Description=Node Exporter
+Description=Prometheus Node Exporter
 After=network.target
 
 [Service]
