f358c04aed01a2d96817729b9cb3aae227d04c89
Divyansh <divyanshtiwarijpr@gmail.com>
2023-07-25 11:59:00 +0530

    UPDATED

--- gitlab-ubuntu-22.04/gitlab-install.sh
+++ gitlab-ubuntu-22.04/gitlab-install.sh
@@ -9,7 +9,7 @@ sudo apt dist-upgrade -y
 sudo apt install -y curl openssh-server ca-certificates tzdata perl
 
 # Add the GitLab package repository and install the package
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
 
 sudo EXTERNAL_URL="https://gitlab.divyansh.net" apt-get install -y gitlab-ce 
